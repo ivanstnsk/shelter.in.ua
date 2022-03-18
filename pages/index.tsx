2a3daed8adb6dc694488133c220520a9437d66f4
stnsk <stnsk@MacBook-Pro-stnsk.local>
2022-03-18 02:41:53 +0200

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          IN DEVELOPMENT
+          Головна сторінка у розробці
         </h1>
       </main>
 
