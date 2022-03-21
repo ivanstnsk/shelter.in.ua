b37a00b5769f5f3a85a605b35a400b9e9bb00384
ivanstnsk <ivan.stnsk@gmail.com>
2022-03-21 23:19:58 +0200

    Add subdomain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites() {
+    return {
+        beforeFiles: [
+            // if the host is `app.acme.com`,
+            // this rewrite will be applied
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'map.shelter.in.ua',
+                    },
+                ],
+                destination: '/map/:path*',
+            },
+        ]
+    }
+}
 }
 
 module.exports = nextConfig
